e0faf1be65b4a51bbb20b1d98b1e292b04251fdb
josegr16 <jdgr961@gmail.com>
2024-12-22 14:35:00 -0400

    Modifique los numeros entre 1 y 9

--- parts/numeros.php
+++ parts/numeros.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h3 class="mb-4 numeros">Elige un número entre 1 y 10</h3>
+                <h3 class="mb-4 numeros">Elige un número entre 1 y 9git</h3>
                 <div class="col-md-8">
                 </div>
             </div>

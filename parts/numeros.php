76406fd2646c5a25d181fb17dad5b4f37090353e
josegr16 <jdgr961@gmail.com>
2024-12-22 14:31:44 -0400

    Modifique el archivo numeros y cambie el texto

--- parts/numeros.php
+++ parts/numeros.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h3 class="mb-4 numeros">Elige un número o más</h3>
+                <h3 class="mb-4 numeros">Elige un número entre 1 y 10</h3>
                 <div class="col-md-8">
                 </div>
             </div>

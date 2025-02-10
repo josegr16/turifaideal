b16e0c6dca033c17383d94a3a167b5c9eb77c073
josegr16 <jdgr961@gmail.com>
2025-02-10 16:20:32 -0400

    metodos

--- parts/metodos_pago.php
+++ parts/metodos_pago.php
@@ -1,11 +1,33 @@
-<section class="metodos_pago">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h3 class="mb-4 metodos">Metodos de pago</h3>
-
-            </div>
-        
-        </div>
-    </div>
+<section class="metodos_pago">
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-md-12">
+
+                <h3 class="mb-4 metodos">Metodos de pago</h3>
+
+                <!--Metodos de pago -->
+            <div class="Metodos-pago-texto">
+                <div class="metodo">
+                    <p class="metodo-nombre">Pago Móvil</p>
+                    <p class="metodo-descripcion">J-317619447, 04121636961, Banesco.</p>
+                </div>
+                <div class="metodo">
+                    <p class="metodo-nombre">Binance</p>
+                    <p class="metodo-descripcion">derwinmart90@gmail.com</p>
+                </div>
+                <div class="metodo">
+                    <p class="metodo-nombre">Zelle</p>
+                    <p class="metodo-descripcion">Nestor Millano, Nestordanielmillanoortiz@gmail.com</p>
+                </div>
+            </div>
+
+        
+
+        </div>
+
+    </div>
+
 </section>
